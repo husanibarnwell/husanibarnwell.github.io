b9d33139452490f0b455933df2cad61be7207aee
Husani K. Barnwell <hbarnwel@new-host-7.home>
2018-02-19 16:46:55 -0500

    not sure but here goes

--- labs/lab-4b/assets/js/app.js
+++ labs/lab-4b/assets/js/app.js
@@ -7,4 +7,38 @@ $('.item').click(function() {
     $('.item-count').html(new_count);
     $(this).addClass('in-cart');
   }
+
+  var vid = document.getElementById("bgvid");
+var pauseButton = document.querySelector("#polina button");
+
+if (window.matchMedia('(prefers-reduced-motion)').matches) {
+    vid.removeAttribute("autoplay");
+    vid.pause();
+    pauseButton.innerHTML = "Paused";
+}
+
+function vidFade() {
+  vid.classList.add("stopfade");
+}
+
+vid.addEventListener('ended', function()
+{
+// only functional if "loop" is removed 
+vid.pause();
+// to capture IE10
+vidFade();
+}); 
+
+
+pauseButton.addEventListener("click", function() {
+  vid.classList.toggle("stopfade");
+  if (vid.paused) {
+    vid.play();
+    pauseButton.innerHTML = "Pause";
+  } else {
+    vid.pause();
+    pauseButton.innerHTML = "Paused";
+  }
+})
+
 });
